e48f1954011e3a6d897218219daf415b132e6337
GuilhermeFreire01 <guilhermelima488@gmail.com>
2024-09-18 19:16:22 -0300

    adicionando checkbutton

---
+++ checkbutton_tk.py
@@ -0,0 +1,15 @@
+import tkinter as tk
+
+def verificar():
+    if opcao.get():
+        print("Selecionado")
+    else:
+        print("Não selecionado")
+
+janela = tk.Tk()
+janela.title("Exemplo simples de checkbutton")
+opcao = tk.IntVar()
+check = tk.Checkbutton(janela, text="Escolha esta opção",
+                       variable=opcao, command=verificar)
+check.pack(pady=10)
+janela.mainloop()
